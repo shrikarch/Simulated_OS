815e92cacb0b17b3c0e36d3773f530c7b64e8683
shrikarch <shrikar.code@gmail.com>
2016-03-21 23:09:04 -0500

    Added prompt and callback. Prompt now adds to DLL

--- app.js
+++ app.js
@@ -2,14 +2,36 @@ var prompt = require('prompt');
 var DLL = require('doubly-linked-list');
 var list = new DLL.DoublyLinkedList();
 
-var objectA = {
-    pid: 123,
-    lux: "non"
-}
-list.append(objectA);
-node = list.append('data2');
-list.append('data3');
+function printPromt(callback){
+    prompt.start(); //initiate the input from the user.
+    // Get properties for PCB
+    prompt.get(['processId', 'duration', 'priority'], function (err, result) {
+        console.log('Command-line input received:');
+        console.log('  Process ID: ' + result.processId);
+        console.log('  Process Duration: ' + result.duration);
+        console.log('  Process priority: ' + result.priority);
+        var pcb = {
+            pid: result.processId,
+            duration: result.duration,
+            priority: result.priority
+        };
+        callback(pcb);
+    });
 
-var nodey = list.item(0);
-console.log(list._length);
-console.log(nodey.data.pid);
+};
+
+function addToLinkedList(pcb){
+    //if (err) console.error('Error Handling LinkedList', err);
+    //else{
+        list.append(pcb);
+        node = list.append('data2');
+        list.append('data3');
+
+        var nodey = list.item(0);
+        //console.log(list._length);
+        console.log("PID: " + nodey.data.pid + " Duration: " + nodey.data.duration);
+    //};
+
+};
+
+printPromt(addToLinkedList);
